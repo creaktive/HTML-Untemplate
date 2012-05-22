ed0015a57a436d8bd35ef7dba78f17104bc7919b
Stanislaw Pusep <stanislav.poussep@buscapecompany.com>
2012-05-22 14:55:03 -0300

    use the GitHub-hosted CSS

--- lib/HTML/Linear/Path/Colors.pm
+++ lib/HTML/Linear/Path/Colors.pm
@@ -76,7 +76,7 @@ our @html = (
 <head>
 <title></title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<link rel="stylesheet" href="highlight.css" type="text/css">
+<link rel="stylesheet" href="http://creaktive.github.com/HTML-Untemplate/highlight.css" type="text/css">
 </head>
 <body>
 <table summary="">
